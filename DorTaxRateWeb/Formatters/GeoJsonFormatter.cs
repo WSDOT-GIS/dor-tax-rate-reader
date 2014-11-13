79f499f301786900574a3963fdf800f79a409659
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-11-13 09:33:55 -0800

    Added doc comments :page_facing_up:

--- DorTaxRateWeb/Formatters/GeoJsonFormatter.cs
+++ DorTaxRateWeb/Formatters/GeoJsonFormatter.cs
@@ -16,6 +16,9 @@ namespace DorTaxRateWeb.Formatters
 	/// </summary>
 	public class GeoJsonFormatter : BufferedMediaTypeFormatter
 	{
+		/// <summary>
+		/// Creates a new instance of this object.
+		/// </summary>
 		public GeoJsonFormatter()
 		{
 			SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.geo+json"));
@@ -23,11 +26,22 @@ namespace DorTaxRateWeb.Formatters
 			SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/json"));
 		}
 
+		/// <summary>
+		/// Determines if a type can be read.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns><see langword="false"/>.</returns>
+		/// <remarks>This class cannot read any types</remarks>
 		public override bool CanReadType(Type type)
 		{
 			return false;
 		}
 
+		/// <summary>
+		/// Determines if a type can be written by this class.
+		/// </summary>
+		/// <param name="type">A type.</param>
+		/// <returns>Returns a <see cref="bool"/> indicating if the type is supported for writing.</returns>
 		public override bool CanWriteType(Type type)
 		{
 			if (type == typeof(FeatureCollection))
@@ -40,6 +54,13 @@ namespace DorTaxRateWeb.Formatters
 			}
 		}
 
+		/// <summary>
+		/// Serializes the object to the stream as GeoJson.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="value"></param>
+		/// <param name="writeStream"></param>
+		/// <param name="content"></param>
 		public override void WriteToStream(Type type, object value, Stream writeStream, HttpContent content)
 		{
 			using (var writer = new StreamWriter(writeStream) { AutoFlush = true })

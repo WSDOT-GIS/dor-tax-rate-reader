efddaac507f10a0e64ab2c40ce59707adcacc8be
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-11-17 13:41:12 -0800

    Added URL support for out SR
    Output spatial reference system URL parameter now supported. Projection
    code is not yet implemented, though. :warning:

--- DorTaxRateWeb/Controllers/TaxRateController.cs
+++ DorTaxRateWeb/Controllers/TaxRateController.cs
@@ -23,6 +23,7 @@ namespace Wsdot.Dor.Tax.Web.Controllers
 	public class TaxRateController : ApiController
 	{
 		const int _defaultCache = 365*24*60*60*60;
+		const int _defaultSrid = 2927;
 
 		/// <summary>
 		/// Gets the tax rates for a specific quarter-year year.
@@ -30,6 +31,7 @@ namespace Wsdot.Dor.Tax.Web.Controllers
 		/// <param name="year">A year. Minimum allowed value is 2008.</param>
 		/// <param name="quarter">An integer representing a quarterYear: a value of 1 through 4. For 2008, only quarters 3 and 4 are available.</param>
 		/// <returns>Returns a list of <see cref="TaxRateItem"/> objects.</returns>
+		[Route("rates/{year:min(2008)}/{quarter:range(1,4)}")]
 		[Route("rates/{year:min(2008)}/{quarter:range(1,4)}")]
 		[CacheOutput(ServerTimeSpan=_defaultCache, ClientTimeSpan=_defaultCache)]
 		public IEnumerable<TaxRateItem> GetTaxRates(int year, int quarter)
@@ -57,10 +59,12 @@ namespace Wsdot.Dor.Tax.Web.Controllers
 		/// </summary>
 		/// <param name="year">A year. Minimum allowed value is 2008.</param>
 		/// <param name="quarter">An integer representing a quarterYear: a value of 1 through 4. For 2008, only quarters 3 and 4 are available.</param>
+		/// <param name="outSR">The EPSG identifier for a coordinate system.</param>
 		/// <returns>Returns a GeoJSON FeatureCollection.</returns>
 		[Route("boundaries/{year:min(2008)}/{quarter:range(1,4)}")]
+		[Route("boundaries/{year:min(2008)}/{quarter:range(1,4)}/{outSR:int}")]
 		[CacheOutput(ServerTimeSpan = _defaultCache, ClientTimeSpan = _defaultCache)]
-		public FeatureCollection GetSalesTaxJursitictionBoundaries(int year, int quarter)
+		public FeatureCollection GetSalesTaxJursitictionBoundaries(int year, int quarter, int outSR=_defaultSrid)
 		{
 			var boundaries = DorTaxRateReader.EnumerateLocationCodeBoundaries(new QuarterYear(year, quarter));
 			var featureCollection = new FeatureCollection();
@@ -76,12 +80,14 @@ namespace Wsdot.Dor.Tax.Web.Controllers
 		/// <summary>
 		/// Gets current juristiction boundaries by redirecting to the current quarter's juristiction boundaries endpoint.
 		/// </summary>
-		/// <returns>An <see cref="HttpResponseMessage"/> that redirects to <see cref="GetSalesTaxJursitictionBoundaries(int, int)"/>.</returns>
+		/// <returns>An <see cref="HttpResponseMessage"/> that redirects to <see cref="GetSalesTaxJursitictionBoundaries(int, int, int)"/>.</returns>
 		[Route("boundaries")]
-		public HttpResponseMessage GetCurrentSalesTaxJuristictionBoundaries()
+		[Route("boundaries/current")]
+		[Route("boundaries/current/{outSR:int}")]
+		public HttpResponseMessage GetCurrentSalesTaxJuristictionBoundaries(int outSR=_defaultSrid)
 		{
 			var qy = QuarterYear.Current;
-			string newUrl = this.Request.RequestUri.ToString().TrimEnd('/') + string.Format("/{0}/{1}", qy.Year, qy.Quarter);
+			string newUrl = this.Request.RequestUri.ToString().Replace("current", string.Empty).TrimEnd('/') + string.Format("/{0}/{1}/{2}", qy.Year, qy.Quarter, outSR);
 			var response = this.Request.CreateResponse(System.Net.HttpStatusCode.Redirect);
 			response.Headers.Location = new Uri(newUrl);
 			return response;

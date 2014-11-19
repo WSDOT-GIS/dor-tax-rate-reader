8c9d1d324af64a0609120c0d971cdc780363f3de
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2014-11-19 11:39:45 -0800

    Added ability to omit fields

--- DorTaxRateWeb/Extensions.cs
+++ DorTaxRateWeb/Extensions.cs
@@ -9,12 +9,16 @@ using DotSpatial.Topology;
 
 namespace Wsdot.Dor.Tax.Web
 {
+	/// <summary>
+	/// Provides extension methods.
+	/// </summary>
 	public static class Extensions
 	{
 		/// <summary>
 		/// Converts a <see cref="DataRow"/> into an <see cref="AttributesTable"/>.
 		/// </summary>
 		/// <param name="dataRow">A <see cref="DataRow"/></param>
+		/// <param name="omittedFields">Names of fields that will be omitted.</param>
 		/// <returns>An <see cref="AttributesTable"/> containing the data in the input <see cref="DataRow"/>.</returns>
 		public static AttributesTable ToAttributesTable(this DataRow dataRow, IEnumerable<string> omittedFields=null)
 		{
@@ -31,7 +35,14 @@ namespace Wsdot.Dor.Tax.Web
 			return attributesTable;
 		}
 
-		public static FeatureCollection ToNtsFeatureCollection(this IEnumerable<DotSpatial.Data.IFeature> boundaries, int outSR, IEnumerable<string> omittedFields = null)
+		/// <summary>
+		/// Converts DotSpatial features into a NetTopologySuite feature collection.
+		/// </summary>
+		/// <param name="features">DotSpatial features.</param>
+		/// <param name="outSR">The EPSG identifier for a spatial reference system.</param>
+		/// <param name="omittedFields">Names of fields that will be omitted.</param>
+		/// <returns></returns>
+		public static FeatureCollection ToNtsFeatureCollection(this IEnumerable<DotSpatial.Data.IFeature> features, int outSR, IEnumerable<string> omittedFields = null)
 		{
 			var featureCollection = new FeatureCollection();
 			// Omit CRS for WGS 84, which is the default for GeoJSON.
@@ -39,12 +50,9 @@ namespace Wsdot.Dor.Tax.Web
 			{
 				featureCollection.CRS = new NamedCRS(string.Format("urn:ogc:def:crs:EPSG::{0}", outSR));
 			}
-			// Get the index of the LOCCODE column.
-			int locCodeColumn = boundaries.First().DataRow.Table.Columns["LOCCODE"].Ordinal;
-			foreach (var boundary in boundaries)
+			foreach (var boundary in features)
 			{
 				var geometry = boundary.ToShape().ToGeoAPI();
-				var locCode = (string)boundary.DataRow[locCodeColumn];
 				var attributesTable = boundary.DataRow.ToAttributesTable(omittedFields);
 				var feature = new Feature(geometry, attributesTable);
 				featureCollection.Add(feature);
